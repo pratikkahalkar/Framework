931b5d0b6d6bc5a5c0e5c9434ab77eca9510029d
pratikkahalkar <getkahalkar@gmail.com>
2021-09-02 11:53:18 +0530

    Update LoginPage.java
    Comment added

--- Framework/src/test/java/com/framework/pages/LoginPage.java
+++ Framework/src/test/java/com/framework/pages/LoginPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-// Login page
+// Login page demo
 
 public class LoginPage {
 
